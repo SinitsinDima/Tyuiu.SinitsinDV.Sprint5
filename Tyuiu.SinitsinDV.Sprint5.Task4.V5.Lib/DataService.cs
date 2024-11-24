008d6219678d1580620a3f7c1b6bba0aa501d734
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-11-24 19:58:19 +0500

    Task4

--- Tyuiu.SinitsinDV.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint5.Task4.V5.Lib/DataService.cs
@@ -1,12 +1,15 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Globalization;
 namespace Tyuiu.SinitsinDV.Sprint5.Task4.V5.Lib
 {
     public class DataService : ISprint5Task4V5
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
+            
+            string strX = File.ReadAllText(path).Replace(',', '.');
+            double x = Convert.ToDouble(strX, CultureInfo.InvariantCulture);
             double res = Math.Round((4.26 * Convert.ToDouble(strX)) / Math.Sin(Convert.ToDouble(strX)), 3);
             return res;
         }
