168a6235153210c9f6b6f3fc50527df219fbb6e6
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-11-25 19:32:22 +0500

    Task5

--- Tyuiu.SinitsinDV.Sprint5.Task5.V12.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint5.Task5.V12.Lib/DataService.cs
@@ -3,23 +3,25 @@ using System.IO;
 using System.Text;
 using System.Numerics;
 using Microsoft.Win32.SafeHandles;
+using System.Globalization;
 namespace Tyuiu.SinitsinDV.Sprint5.Task5.V12.Lib
 {
     public class DataService : ISprint5Task5V12
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path).Replace('.', ',');
+            string paths = File.ReadAllText(path).Replace('.', ',');
+            double x = Convert.ToDouble(paths, CultureInfo.InvariantCulture);
 
             double res = 0;
             double plus = 0;
-            string line;
+            
             double minus = 0;
             using (StreamReader reader = new StreamReader(path))
             {
 
-                
 
+                string line;
                 while ((line = reader.ReadLine()) != null)
                 {
 
