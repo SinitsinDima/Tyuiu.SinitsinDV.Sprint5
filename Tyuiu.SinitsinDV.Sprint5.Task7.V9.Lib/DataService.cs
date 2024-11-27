fecd7f7612dabf0e0a8abc714e5a8ca543279244
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-11-27 22:47:50 +0500

    Task7

--- Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib/DataService.cs
@@ -8,26 +8,26 @@ namespace Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            // Путь для сохранения выходного файла
+            
             string[] fullpath = { @"c:\DataSprint5", "OutPutDataFileTask7V9.txt" };
             string fullSaveFile = Path.Combine(fullpath);
 
-            // Удаляем выходной файл, если он существует
+            
             if (File.Exists(fullSaveFile))
             {
                 File.Delete(fullSaveFile);
             }
 
-            // Читаем строки и обрабатываем их
+            
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    // Удаляем заглавные латинские буквы
+                    
                     string processedLine = Regex.Replace(line, "[A-Z]", string.Empty);
 
-                    // Записываем обработанную строку в выходной файл
+                    
                     File.AppendAllText(fullSaveFile, processedLine + Environment.NewLine);
                 }
             }
