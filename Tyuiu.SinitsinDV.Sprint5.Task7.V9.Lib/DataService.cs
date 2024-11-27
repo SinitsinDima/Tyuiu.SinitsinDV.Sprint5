9a9a41a766dfe9b7188ce3b893cc1feacd827aaa
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-11-27 23:17:10 +0500

    Task7

--- Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib/DataService.cs
@@ -8,16 +8,18 @@ namespace Tyuiu.SinitsinDV.Sprint5.Task7.V9.Lib
         public string LoadDataAndSave(string path)
         {
 
+            
             string[] fullpath = { @"c:\DataSprint5", "OutPutDataFileTask7V9.txt" };
             string fullSaveFile = Path.Combine(fullpath);
+            string path2 = $@"{Path.GetTempPath()}OutPutDataFileTask7V9.txt";
             FileInfo fileInfo = new FileInfo(fullSaveFile);
 
-            if (File.Exists(fullSaveFile))
+            if (fileInfo.Exists)
             {
                 File.Delete(fullSaveFile);
             }
 
-
+            
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
